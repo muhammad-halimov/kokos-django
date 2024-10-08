7e46c8514c4ff2dcf7a09ef11797bdbe7eefbc27
winted7331 <kirill.d.ovchinnikov@gmail.com>
2024-10-08 08:19:13 +0400

    add ER

--- base/processors.py
+++
@@ -1,5 +0,0 @@
-from . import models
-
-
-def header_menu(request):
-    return {'header_menu': models.HeaderModel.objects.all()}
--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('authorize/', views.authorize, name='authorize'),
-    path('register/', views.register, name='register'),
-    path('logout/', views.logout_page, name='logout'),
-]
