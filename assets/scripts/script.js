7949c0bb3339b53d7e283fe24c782eaa9a810ec9
MarilovaPolina <marilovap@mail.ru>
2024-10-14 14:04:39 +0400

    team.html, названия постов выводятся на главную

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
 const blocks = document.querySelectorAll('.one_news');
 const sections = document.querySelectorAll('.section');
 const introImg = document.getElementById('introImg');
+const newsName = document.querySelector('.news_name');
+const newsDescription = document.querySelector('.news_description');
 
 let currentIndex = 0;
 let interval;
@@ -10,7 +12,12 @@ let currentNewsLink = '';
 function updateIntroImg() {
   const images = Array.from(blocks).map(block => block.querySelector('img').src);
   const links = Array.from(blocks).map(block => block.querySelector('a').href);
+  const names = Array.from(blocks).map(block => block.querySelector('p').textContent);
+  const descriptions = Array.from(blocks).map(block => block.querySelector('p').getAttribute('data-description'));
+
   introImg.src = images[currentIndex];
+  newsName.textContent = names[currentIndex];
+  newsDescription.textContent = descriptions[currentIndex];
   currentNewsLink = links[currentIndex];
   introImg.classList.add('fade-out');
 
@@ -35,12 +42,16 @@ function resetInterval() {
 
 blocks.forEach((block, index) => {
   block.addEventListener('click', (event) => {
-    event.preventDefault(); // Предотвратить переход по ссылке
+    event.preventDefault();
     introImg.classList.add('fade-out');
+    const name = block.querySelector('p').textContent;
+    const description = block.querySelector('p').getAttribute('data-description');
     currentNewsLink = block.querySelector('a').href;
 
     setTimeout(() => {
       introImg.src = block.querySelector('img').src;
+      newsName.textContent = name;
+      newsDescription.textContent = description;
       introImg.classList.remove('fade-out');
       introImg.classList.add('fade-in');
       sections.forEach((section, i) => {
